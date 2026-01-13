93f575e9d985261770fe4417a6661b54a1eeeac8
ScridID <danisw441@gmail.com>
2026-01-13 17:34:59 +0700

    IntentPratikum

---
+++ app/src/main/java/com/example/intentpratikum/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.intentpratikum;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnOpenSecond = findViewById(R.id.btnOpenSecond);
+
+        btnOpenSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
